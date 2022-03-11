e735e14c5538a6026803af64f1af07af7ebc4397
funkam <88379260+funkam@users.noreply.github.com>
2022-03-11 13:00:32 +0100

    Delete data-raw directory
    not needed anymore

--- data-raw/archive.R
+++
@@ -1,5 +0,0 @@
-## code to prepare `archive` dataset goes here
-archive_backup<-read.csv("C:/Users/funkalexa/Documents/MoonShiny/local/MoonNMR/data-raw/archive/archive.csv")
-usethis::use_data(archive, overwrite = TRUE)
-usethis::use_data(archive_backup, overwrite = TRUE)
-
--- data-raw/examples.R
+++
@@ -1,9 +0,0 @@
-## code to prepare `examples` dataset goes here
-experiments<-read.csv("C:/Users/funkalexa/Documents/MoonShiny/local/MoonNMR/example_files/Format_Experiments.csv")
-usethis::use_data(experiments, overwrite = TRUE)
-
-
-templateuploader<-read.csv("C:/Users/funkalexa/Documents/MoonShiny/local/MoonNMR/example_files/Format_for_templateuploader.csv")
-usethis::use_data(templateuploader, overwrite = TRUE)
-
-
