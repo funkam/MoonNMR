99073f2a8becc567e07c248f9f79ad5b524f6d0a
funkam <88379260+funkam@users.noreply.github.com>
2022-03-11 13:00:01 +0100

    Add files via upload
    Updated how archiving works

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -15,7 +15,7 @@
 
 ## Dependencies ----
 ## Add one line by package you want to add as dependency
-usethis::use_package( "shinydashboard")
+usethis::use_package("shinydashboard")
 usethis::use_package("shinyWidgets")
 usethis::use_package("plotly")
 usethis::use_package("janitor")
